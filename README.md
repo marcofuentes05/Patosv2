b94bc3754302a9487cc70e61bec53c28910c3385
Marco Fuentes <marcofuentes1607@gmail.com>
2019-05-24 11:48:28 -0600

    Ya mero

--- README.md
+++ README.md
@@ -21,9 +21,25 @@ Para poder usar el programa, es necesario instalar py2neo usando Pip:
 
   ```pip install py2neo```
   
-Ademas, la base de datos del proyecto ha de ser la única que esté abierta y la contraseña se especifica en las primeras lineas del programa.
+Ademas, la base de datos del proyecto ha de ser la única que esté abierta y la contraseña se especifica en las primeras lineas del programa. La contraseña de la base de datos usada para las pruebas es patos123.
 
 El nombre del programa principal es ```main.py```. El archivo que almacena las clases (modelo de datos) ```Clases.py```.
+
+## Algoritmos Usados
+
+El algoritmo de recomendación implementado es una mezcla entre varios algoritmos de recomendación sencillos.
+
+### Recomendación por gustos
+
+	El algoritmo busca todos los lugares que cumplen con al menos una de las preferencias de viajes. 
+
+### Recomendación por mis amigos
+
+	El algoritmo busca las preferencias de los amigos del usuario, asi como tambien los lugares que ha visitado, y los agrega a la lista de recomendaciones
+
+### Recomendacion ponderada
+
+	Cada elemento en la lista de recomendaciones tiene un atributo contador, de manera que este contador se incrementa cada vez que ese destino aparece en la lista de recomendaciones. De esta forma, los lugares que cumplen con las preferencias del usuario tendran una ponderación mayor a los que no, pero que siguen en la lista.
 
 
 ## Datos
--- main.py
+++ main.py
@@ -13,12 +13,12 @@ from py2neo import *
 
 #Version Adaptada de bubbleSort. Original de https://interactivepython.org/runestone/static/pythonds/SortSearch/TheBubbleSort.html
 def bubbleSort(alist):
-    for passnum in range(len(alist)-1,0,-1):
-        for i in range(passnum):
-            if alist[i].contador>alist[i+1].contador:
-                temp = alist[i]
-                alist[i] = alist[i+1]
-                alist[i+1] = temp
+	for passnum in range(len(alist)-1,0,-1):
+		for i in range(passnum):
+			if alist[i].contador>alist[i+1].contador:
+				temp = alist[i]
+				alist[i] = alist[i+1]
+				alist[i+1] = temp
 
 #Esta funcion nos permite saber si un lugar ya esta en la lista de recomendaciones o no.
 #Devuelve un entero con la posicion del elemento, y -1 si no esta en la lista.
@@ -32,180 +32,191 @@ def estaEnLista(lista,nombre):
 #-------------------------------------------------------
 # EN LA LINEA DE ABAJO SE PONE LA CONTRASEÑA DE LA BASE DE DATOS
 graph = Graph (password = "patos123")
-"""                             ^
-                                |
-                                |
-                                |
-                                |
-                                |
-                                |
-                                |
-                                |
-                                |
+"""							 ^
+								|
+								|
+								|
+								|
+								|
+								|
+								|
+								|
+								|
 --------------------------------
 """
 # EN LA LINEA DE ARRIBA SE PONE LA CONTRASEÑA DE LA BASE DE DATOS
 #-------------------------------------------------------
 string = """
- /$$$$$$$$ /$$$$$$$  /$$$$$$ /$$    /$$  /$$$$$$   /$$$$$$   /$$$$$$
-|__  $$__/| $$__  $$|_  $$_/| $$   | $$ /$$__  $$ /$$__  $$ /$$__  $$
-   | $$   | $$  \ $$  | $$  | $$   | $$| $$  \ $$| $$  \__/| $$  \ $$
-   | $$   | $$$$$$$/  | $$  |  $$ / $$/| $$$$$$$$| $$ /$$$$| $$  | $$
-   | $$   | $$__  $$  | $$   \  $$ $$/ | $$__  $$| $$|_  $$| $$  | $$
-   | $$   | $$  \ $$  | $$    \  $$$/  | $$  | $$| $$  \ $$| $$  | $$
-   | $$   | $$  | $$ /$$$$$$   \  $/   | $$  | $$|  $$$$$$/|  $$$$$$/
-   |__/   |__/  |__/|______/    \_/    |__/  |__/ \______/  \______/
+ /$$$$$$$$ /$$$$$$$  /$$$$$$ /$$    /$$   /$$$$$$   /$$$$$$   /$$$$$$
+|__  $$__/| $$__  $$|_  $$_/| $$   | $$  /$$__  $$ /$$__  $$ /$$__  $$
+   | $$   | $$  \ $$  | $$  | $$   | $$ | $$  \ $$| $$  \__/| $$  \ $$
+   | $$   | $$$$$$$/  | $$  |  $$ / $$/ | $$$$$$$$| $$ /$$$$| $$  | $$
+   | $$   | $$__  $$  | $$   \  $$ $$/  | $$__  $$| $$|_  $$| $$  | $$
+   | $$   | $$  \ $$  | $$    \  $$$/   | $$  | $$| $$  \ $$| $$  | $$
+   | $$   | $$  | $$ /$$$$$$   \  $/    | $$  | $$|  $$$$$$/|  $$$$$$/
+   |__/   |__/  |__/|______/    \_/	|__/  |__/ \______/  \______/
 """
 sigue0 = True
 while sigue0:
-    print(string)
-    #Login (no es seguro, pero funciona bien) :D
-    nombre = input ("Hola! ¿Cual es tu nombre? (Ingresa X para salir del programa)\n")
-    #Hago esto para asegurarme que no voy a hacer pull de un objeto vacio
-    d = graph.run("MATCH (n:Persona) WHERE n.nombre = '"+nombre+"' RETURN n").data()
-    if len(d)>0:
-        #Instancio una persona para que sirva de enlace entre mi programa y el grafo
-        p = Persona()
-        p.nombre = nombre
-        #listaLugares tiene objetos Lugar
-        graph.pull(p)
-        sigue = True
-        while sigue:
-            #Viva la Libertad
-            des1 = input("Hola, "+nombre+"\n\t1. Tus gustos \n\t2. Tus Amigos\n\t3. Lugares visitados\n\t4. Sugerencia de Viaje\n\t5. Agregar lugar visitado\n\t6. Eliminar Amigo\n\t7.Salir\n")
-            if (des1 =="1"):
-                for gusto in p.likes:
-                    print(gusto.cualidad)
-            elif(des1=="2"):
-                for amigo in p.amigos:
-                    print(amigo.nombre)
-            elif(des1=="3"):
-                cOp=input("Deseas ver... \n\t1. Ciudades\n\t2. Paises")
-                if cOp =="1":
-                    if (len(p.yaVisito)!=0):
-                        for l12 in p.yaVisito:
-                            print(l12.nombre)
-                    else:
-                        print("No has visitado ningun lugar :(")
-                elif cOp== "2":
-                    if (len(p.yaVisitoP)!=0):
-                        for l12 in p.yaVisitoP:
-                            print(l12.nombre)
-                    else:
-                        print("No has visitado ningun lugar :(")
-                else:
-                    print("Ese no es un dato valido....")
-            elif(des1=="4"):
-                    des = input ("¿Que deseas que te recomiente?\n\t1. Un pais\n\t2. Una ciudad\n")
-                    listaLugares = []
-                    if (des == "1"):
-                    	#Valoro los intereses del usuario
-                    	for like in p.likes:
-                    		for pais in like.esP:
-                    			numero = estaEnLista(listaLugares,pais.nombre)
-                    			if (numero != -1):
-                    				listaLugares[numero].sumar(5)
-                    			else:
-                    				temp = Lugar(pais.nombre)
-                    				temp.sumar(5)
-                    				listaLugares.append(temp)
-                    	#Valoro los lugares visitados por los amigos
-                    	for amigo in p.amigos:
-                    		for paisito in amigo.yaVisitoP:
-                    			numero = estaEnLista(listaLugares,paisito.nombre)
-                    			if numero != -1:
-                    				listaLugares[numero].sumar(3)
-                    			else:
-                    				temp = Lugar(paisito.nombre)
-                    				temp.sumar(3)
-                    				listaLugares.append(temp)
-                    	bubbleSort(listaLugares)
-                    	listaLugares.reverse()
-                    	for i in listaLugares:
-                    		print("\t"+i.nombre+" puntuacion: "+str(i.contador))
+	print(string)
+	#Login (no es seguro, pero funciona bien) :D
+	nombre = input ("Hola! ¿Cual es tu nombre? (Ingresa X para salir del programa)\n")
+	#Hago esto para asegurarme que no voy a hacer pull de un objeto vacio
+	d = graph.run("MATCH (n:Persona) WHERE n.nombre = '"+nombre+"' RETURN n").data()
+	if len(d)>0:
+		#Instancio una persona para que sirva de enlace entre mi programa y el grafo
+		p = Persona()
+		p.nombre = nombre
+		#listaLugares tiene objetos Lugar
+		graph.pull(p)
+		sigue = True
+		while sigue:
+			#Viva la Libertad
+			des1 = input("Hola, "+nombre+"\n\t1. Tus gustos \n\t2. Tus Amigos\n\t3. Lugares visitados\n\t4. Sugerencia de Viaje\n\t5. Agregar lugar visitado\n\t6. Eliminar Amigo\n\t7.Salir\n")
+			if (des1 =="1"):
+				for gusto in p.likes:
+					print(gusto.cualidad)
+			elif(des1=="2"):
+				for amigo in p.amigos:
+					print(amigo.nombre)
+			elif(des1=="3"):
+				cOp=input("Deseas ver... \n\t1. Ciudades\n\t2. Paises\n")
+				if cOp =="1":
+					if (len(p.yaVisito)!=0):
+						for l12 in p.yaVisito:
+							print(l12.nombre)
+					else:
+						print("No has visitado ningun lugar :(")
+				elif cOp== "2":
+					if (len(p.yaVisitoP)!=0):
+						for l12 in p.yaVisitoP:
+							print(l12.nombre)
+					else:
+						print("No has visitado ningun lugar :(")
+				else:
+					print("Ese no es un dato valido....")
+			elif(des1=="4"):
+					des = input ("¿Que deseas que te recomiente?\n\t1. Un pais\n\t2. Una ciudad\n")
+					listaLugares = []
+					if (des == "1"):
+						#Valoro los intereses del usuario
+						for like in p.likes:
+							for pais in like.esP:
+								numero = estaEnLista(listaLugares,pais.nombre)
+								if (numero != -1):
+									listaLugares[numero].sumar(5)
+								else:
+									temp = Lugar(pais.nombre)
+									temp.sumar(5)
+									listaLugares.append(temp)
+						#Valoro los lugares visitados por los amigos
+						for amigo in p.amigos:
+							#Lo que han visitado sus amigos
+							for paisito in amigo.yaVisitoP:
+								numero = estaEnLista(listaLugares,paisito.nombre)
+								if numero != -1:
+									listaLugares[numero].sumar(3)
+								else:
+									temp = Lugar(paisito.nombre)
+									temp.sumar(3)
+									listaLugares.append(temp)
+							#Lo que le gusta a sus amigos.
+							for pref in amigo.likes:
+								for lugar in pref.esP:
+									numero = estaEnLista(listaLugares,lugar.nombre)
+									if numero != -1:
+										listaLugares[numero].sumar(1)
+									else:
+										tmp = Lugar (lugar.nombre)
+										tmp.sumar(1)
+										listaLugares.append(tmp)
+						bubbleSort(listaLugares)
+						listaLugares.reverse()
+						for i in listaLugares:
+							print("\t"+i.nombre+" puntuacion: "+str(i.contador))
 
-                    elif (des=="2"):
-                    	#Lo que le gusta
-                    	for like in p.likes:
-                    		for lugar in like.es:
-                    			numero = estaEnLista(listaLugares,lugar.nombre)
-                    			if (numero != -1):
-                    				listaLugares[numero].sumar(5)
-                    			else:
-                    				temp = Lugar(lugar.nombre)
-                    				temp.sumar(5)
-                    				listaLugares.append(temp)
-                    	#Lo que sus amigos visitaron
-                    	for amigo in p.amigos:
-                    		for lugar in amigo.yaVisito:
-                    			numero = estaEnLista(listaLugares,lugar.nombre)
-                    			if numero != -1:
-                    				listaLugares[numero].sumar(3)
-                    			else:
-                    				temp = Lugar(lugar.nombre)
-                    				temp.sumar(3)
-                    				listaLugares.append(temp)
-                    		#Y lo que le gusta a sus amigos
-                    		for pref in amigo.likes:
-                    			for lugar in pref.es:
-                    				numero = estaEnLista(listaLugares,lugar.nombre)
-                    				if numero != -1:
-                    					listaLugares[numero].sumar(1)
-                    				else:
-                    					tmp = Lugar (lugar.nombre)
-                    					tmp.sumar(1)
-                    					listaLugares.append(tmp)
+					elif (des=="2"):
+						#Lo que le gusta
+						for like in p.likes:
+							for lugar in like.es:
+								numero = estaEnLista(listaLugares,lugar.nombre)
+								if (numero != -1):
+									listaLugares[numero].sumar(5)
+								else:
+									temp = Lugar(lugar.nombre)
+									temp.sumar(5)
+									listaLugares.append(temp)
+						#Lo que sus amigos visitaron
+						for amigo in p.amigos:
+							for lugar in amigo.yaVisito:
+								numero = estaEnLista(listaLugares,lugar.nombre)
+								if numero != -1:
+									listaLugares[numero].sumar(3)
+								else:
+									temp = Lugar(lugar.nombre)
+									temp.sumar(3)
+									listaLugares.append(temp)
+							#Y lo que le gusta a sus amigos
+							for pref in amigo.likes:
+								for lugar in pref.es:
+									numero = estaEnLista(listaLugares,lugar.nombre)
+									if numero != -1:
+										listaLugares[numero].sumar(1)
+									else:
+										tmp = Lugar (lugar.nombre)
+										tmp.sumar(1)
+										listaLugares.append(tmp)
 
-                    	bubbleSort(listaLugares)
-                    	listaLugares.reverse()
-                    	for i in listaLugares:
-                    		print("\t"+str(i.nombre)+" puntuacion: "+str(i.contador))
-                    	print (str(len(listaLugares)))
-                    else:
-                    	print("Ese valor no es valido :(")
-            elif(des1=="5"):
-                cOp = input("¿Que has visitado?\n\t1. Ciudad\n\t2. Pais\n")
-                if (cOp=="1"):
-                    lugarN = input("¿En donde has estado recientemente?\n")
-                    l = graph.run("MATCH (n:Ciudad) WHERE n.nombre = '"+lugarN+"' RETURN n").data()
-                    if (len(l)>0):
-                        c = Ciudad()
-                        c.nombre=lugarN
-                        graph.pull(c)
-                        p.yaVisito.add(c)
-                        graph.push(p)
-                    else:
-                        print("Ese lugar no esta en la BD :(")
-                elif(cOp=="2"):
-                    lugarN = input("¿En donde has estado recientemente?\n")
-                    c = graph.run("MATCH (n:Pais) WHERE n.nombre = '"+lugarN+"' RETURN n").data()
-                    if (len(l)>0):
-                        c = Pais()
-                        c.nombre=lugarN
-                        graph.pull(c)
-                        p.yaVisitoP.add(c)
-                        graph.push(p)
-                    else:
-                        print("Ese lugar no esta en la BD :(")
-                else:
-                    print("Ese no es un valor valido")
-            elif(des1=="6"):
-                n0 = input("¿Con quien te has peleado, que deseas eliminar de tu lista de amigos?\n")
-                amigo = Persona()
-                amigo.nombre=n0
-                l = graph.run("MATCH (n:Persona) WHERE n.nombre = '"+n0+"' RETURN n").data()
-                if (len(l)>0):
-                    graph.pull(amigo)
-                    p.amigos.remove(amigo)
-                    print("Listo... \n")
-                    graph.push(p)
-            elif(des1=="7"):
-                sigue = False
-                print("Hasta luego!")
-            else:
-                print("Ese no es un valor valido :(")
-    elif(nombre=="X" or "x"):
-        sigue0=False
-        print("Adios ")
-    else:
-        print("Ese nombre no esta registrado :(")
+						bubbleSort(listaLugares)
+						listaLugares.reverse()
+						for i in listaLugares:
+							print("\t"+str(i.nombre)+" puntuacion: "+str(i.contador))
+						print (str(len(listaLugares)))
+					else:
+						print("Ese valor no es valido :(")
+			elif(des1=="5"):
+				cOp = input("¿Que has visitado?\n\t1. Ciudad\n\t2. Pais\n")
+				if (cOp=="1"):
+					lugarN = input("¿En donde has estado recientemente?\n")
+					l = graph.run("MATCH (n:Ciudad) WHERE n.nombre = '"+lugarN+"' RETURN n").data()
+					if (len(l)>0):
+						c = Ciudad()
+						c.nombre=lugarN
+						graph.pull(c)
+						p.yaVisito.add(c)
+						graph.push(p)
+					else:
+						print("Ese lugar no esta en la BD :(")
+				elif(cOp=="2"):
+					lugarN = input("¿En donde has estado recientemente?\n")
+					c = graph.run("MATCH (n:Pais) WHERE n.nombre = '"+lugarN+"' RETURN n").data()
+					if (len(l)>0):
+						c = Pais()
+						c.nombre=lugarN
+						graph.pull(c)
+						p.yaVisitoP.add(c)
+						graph.push(p)
+					else:
+						print("Ese lugar no esta en la BD :(")
+				else:
+					print("Ese no es un valor valido")
+			elif(des1=="6"):
+				n0 = input("¿Con quien te has peleado, que deseas eliminar de tu lista de amigos?\n")
+				amigo = Persona()
+				amigo.nombre=n0
+				l = graph.run("MATCH (n:Persona) WHERE n.nombre = '"+n0+"' RETURN n").data()
+				if (len(l)>0):
+					graph.pull(amigo)
+					p.amigos.remove(amigo)
+					print("Listo... \n")
+					graph.push(p)
+			elif(des1=="7"):
+				sigue = False
+				print("Hasta luego!")
+			else:
+				print("Ese no es un valor valido :(")
+	elif(nombre=="X" or "x"):
+		sigue0=False
+		print("Adios ")
+	else:
+		print("Ese nombre no esta registrado :(")
