ec09e9ef29bb4602a354926d8c7e802f3b5f2d0a
Marco Fuentes <marcofuentes1607@gmail.com>
2019-05-23 21:04:15 -0600

    Funcionalidades Faltantes agregadas

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ while sigue0:
         sigue = True
         while sigue:
             #Viva la Libertad
-            des1 = input("Hola, "+nombre+"\n\t1. Tus gustos \n\t2. Tus Amigos\n\t3. Sugerencia de Viaje\n\t4. Salir\n")
+            des1 = input("Hola, "+nombre+"\n\t1. Tus gustos \n\t2. Tus Amigos\n\t3. Lugares visitados\n\t4. Sugerencia de Viaje\n\t5. Agregar lugar visitado\n\t6. Eliminar Amigo\n\t7.Salir\n")
             if (des1 =="1"):
                 for gusto in p.likes:
                     print(gusto.cualidad)
@@ -65,6 +65,22 @@ while sigue0:
                 for amigo in p.amigos:
                     print(amigo.nombre)
             elif(des1=="3"):
+                cOp=input("Deseas ver... \n\t1. Ciudades\n\t2. Paises")
+                if cOp =="1":
+                    if (len(p.yaVisito)!=0):
+                        for l12 in p.yaVisito:
+                            print(l12.nombre)
+                    else:
+                        print("No has visitado ningun lugar :(")
+                elif cOp== "2":
+                    if (len(p.yaVisitoP)!=0):
+                        for l12 in p.yaVisitoP:
+                            print(l12.nombre)
+                    else:
+                        print("No has visitado ningun lugar :(")
+                else:
+                    print("Ese no es un dato valido....")
+            elif(des1=="4"):
                     des = input ("¿Que deseas que te recomiente?\n\t1. Un pais\n\t2. Una ciudad\n")
                     listaLugares = []
                     if (des == "1"):
@@ -132,7 +148,43 @@ while sigue0:
                     	print (str(len(listaLugares)))
                     else:
                     	print("Ese valor no es valido :(")
-            elif(des1=="4"):
+            elif(des1=="5"):
+                cOp = input("¿Que has visitado?\n\t1. Ciudad\n\t2. Pais\n")
+                if (cOp=="1"):
+                    lugarN = input("¿En donde has estado recientemente?\n")
+                    l = graph.run("MATCH (n:Ciudad) WHERE n.nombre = '"+lugarN+"' RETURN n").data()
+                    if (len(l)>0):
+                        c = Ciudad()
+                        c.nombre=lugarN
+                        graph.pull(c)
+                        p.yaVisito.add(c)
+                        graph.push(p)
+                    else:
+                        print("Ese lugar no esta en la BD :(")
+                elif(cOp=="2"):
+                    lugarN = input("¿En donde has estado recientemente?\n")
+                    c = graph.run("MATCH (n:Pais) WHERE n.nombre = '"+lugarN+"' RETURN n").data()
+                    if (len(l)>0):
+                        c = Pais()
+                        c.nombre=lugarN
+                        graph.pull(c)
+                        p.yaVisitoP.add(c)
+                        graph.push(p)
+                    else:
+                        print("Ese lugar no esta en la BD :(")
+                else:
+                    print("Ese no es un valor valido")
+            elif(des1=="6"):
+                n0 = input("¿Con quien te has peleado, que deseas eliminar de tu lista de amigos?\n")
+                amigo = Persona()
+                amigo.nombre=n0
+                l = graph.run("MATCH (n:Persona) WHERE n.nombre = '"+n0+"' RETURN n").data()
+                if (len(l)>0):
+                    graph.pull(amigo)
+                    p.amigos.remove(amigo)
+                    print("Listo... \n")
+                    graph.push(p)
+            elif(des1=="7"):
                 sigue = False
                 print("Hasta luego!")
             else:
